1a5c86a7c6691f548c25f91cf592ed2d44020906
= <=>
2024-01-08 19:38:15 -0400

    Add file script

---
+++ conexao-html-js/script.js
@@ -0,0 +1,10 @@
+let elemento01 = document.getElementById("#logo");
+let elemento02 = document.getElementsByClassName(".lista_redes");
+let elemento03 = document.querySelector("form");
+let elemento04 = document.querySelectorAll("span");
+
+
+console.log(elemento01);
+console.log(elemento02);
+console.log(elemento03);
+console.log(elemento04);
