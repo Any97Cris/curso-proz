9a71d62fadfcb733f193a1a98827383e74ed12d6
Any97Cris <criscianysilva1997@gmail.com>
2024-01-16 20:19:05 -0400

    exemplo seletores javascript

---
+++ seletores-dom/script.js
@@ -0,0 +1,12 @@
+const titulo = document.getElementById("titulo")
+const titulo2 = document.querySelectorAll("#titulo")
+const texto_simples = document.getElementsByClassName('.texto-simples')
+const texto_simples2 = document.querySelectorAll("texto-simples")
+const segundoTitulo = document.querySelector("div h2")
+
+
+// console.log(texto_simples2)
+// console.log(titulo2)
+// console.log(segundoTitulo)
+// console.log(titulo)
+console.log(texto_simples)
