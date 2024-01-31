b85e2b0be5bd41447b056d9ec671565f3ffde0dc
Any97Cris <criscianysilva1997@gmail.com>
2024-01-30 20:59:19 -0400

    exercicio elemento dom proz

--- exercicio_criar_elemento_dom/script.js
+++ exercicio_criar_elemento_dom/script.js
@@ -1,9 +1,10 @@
+let body = document.querySelector("body");
+let listaItens = document.querySelector(".lista-itens");
 let titulo = document.createElement("h2")
 let elementoLi1 = document.createElement("li");
 let elementoLi2 = document.createElement("li");
 let elementoLi3 = document.createElement("li");
-let listaItens = document.querySelector(".lista-itens");
-let body = document.querySelector("body");
+
 
 //titulo
 titulo.innerHTML = "Produto à venda"
@@ -18,8 +19,7 @@ elementoLi3.innerHTML = "Preço"
 elementoLi3.id = "preco-produto"
 
 //Cria titulo
-body.appendChild(titulo).insertAdjacentElement('beforebegin', listaItens)
-body.insertAdjacentElement('afterbegin', listaItens)
+body.insertBefore(titulo, listaItens);
 
 //Cria elemento li
 listaItens.appendChild(elementoLi1)
@@ -28,4 +28,16 @@ listaItens.appendChild(elementoLi3)
 
 
 
-console.log()
+//titulo
+let tagTitulo = document.createElement("h2");
+tagTitulo.innerHTML = "Produtos à Venda";
+document.body.append(tagTitulo)
+
+//lista
+let tagUl = document.createElement("ul");
+tagUl.innerHTML = `
+    <li id="nome-produto">Nome</li>
+    <li id="desc-produto">Descrição</li>
+    <li id="preco-produto">Preço</li>
+`
+document.body.append(tagUl)
